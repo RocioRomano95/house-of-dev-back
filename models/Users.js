1754cce6436def178f8696babf236b5063f41f33
RocioRomano <rociobromano95@gmail.com>
2023-05-31 11:19:37 -0300

    server running and model user done

--- models/Users.js
+++ models/Users.js
@@ -7,10 +7,18 @@ User.init(
   {
     name: { type: S.STRING, require: true },
     last_name: { type: S.STRING, require: true },
-    email: { type: S.STRING, require: true, validate: { isEmail: true } },
+    email: {
+      type: S.STRING,
+      require: true,
+      validate: { isEmail: true },
+      unique: true,
+    },
     password: { type: S.STRING, require: true, validate: { notEmpty: true } },
+    image: { type: S.STRING },
     phone: { type: S.INTEGER, require: true },
     is_admin: { type: S.BOOLEAN, defaultValue: false },
   },
   { sequelize: db, modelName: "user" }
 );
+
+module.exports = User;
