d5a02a2776cc449d3c522da41d7d5fac8fa3cd8c
RocioRomano <rociobromano95@gmail.com>
2023-06-01 11:35:03 -0300

    credentials

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,6 +2,7 @@ const Users = require("../models/Users");
 
 exports.signup_user = async (req, res) => {
   try {
+    console.log("ReqBody", req.body);
     const searchUser = await Users.findOne({
       where: { email: req.body.email },
     });
@@ -10,8 +11,10 @@ exports.signup_user = async (req, res) => {
       return res.status(400).send("Este usuario ya existe");
     }
     const newUser = await Users.create(req.body);
+    console.log("newuser", newUser);
     res.status(200).send(newUser);
   } catch (error) {
+    console.log("ERROR", error);
     res.send(error);
   }
 };
--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const cookiesParser = require("cookie-parser");
 const app = express();
 
 app.use(cookiesParser());
-app.use(cors({ origin: "http://localhost:5173", credential: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use("/api", router);
 
 const PORT = process.env.SERVER_PORT;
 
-db.sync({ force: true }).then(() => {
+db.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
