2582fac61af0ea82b5d933a0e0073f9b0785a473
brendaConte <brendamconte@gmail.com>
2023-06-08 10:38:08 -0300

    add try and catch in token

--- config/token.js
+++ config/token.js
@@ -3,15 +3,21 @@ const jwt = require("jsonwebtoken");
 const secret = process.env.SECRET;
 
 const generateToken = (payload) => {
-  console.log("payload=>", payload);
-  const token = jwt.sign({ user: payload }, secret, { expiresIn: "5d" });
-  console.log("Token", token);
-  return token;
+  try {
+    const token = jwt.sign({ user: payload }, secret, { expiresIn: "5d" });
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const validateToken = (token) => {
-  const user = jwt.verify(token, secret);
-  return user;
+  try {
+    const user = jwt.verify(token, secret);
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = { generateToken, validateToken };
