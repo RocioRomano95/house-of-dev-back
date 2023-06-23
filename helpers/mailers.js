50bd11b10e8a041ef6d15e6289a75d5ad5804855
jorgefortaleche <jorgefh08@hotmail.com>
2023-06-23 18:15:19 -0300

    nodemailer in progress

---
+++ helpers/mailers.js
@@ -0,0 +1,13 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.office365.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL,
+    Pass: process.env.EMAIL_PASSWORD,
+  },
+});
+module.exports = transporter;
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const {
   edit_user,
 } = require("../controllers/userControllers");
 const { validateAuth } = require("../middlewares");
-const { user_visits } = require("../controllers/visitControllers");
+const { user_visits, sendEmail } = require("../controllers/visitControllers");
 
 router.post("/signup", signup_user);
 router.post("/login", login_user);
@@ -17,5 +17,6 @@ router.get("/me", validateAuth, (req, res) => {
 router.get("/logout", validateAuth, logout_user);
 router.put("/edit-user", validateAuth, edit_user);
 router.get("/user-visits/:userId", user_visits);
+router.post("/sendEmail", sendEmail);
 
 module.exports = router;
